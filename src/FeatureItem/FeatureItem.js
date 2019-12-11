9b198bd5838f597914098d26e783ce115555c138
brezzz <brezzz@users.noreply.github.com>
2019-12-11 15:29:29 -0500

    Update FeatureItem.js

--- src/FeatureItem/FeatureItem.js
+++ src/FeatureItem/FeatureItem.js
@@ -2,12 +2,31 @@ import React, {Component} from 'react';
 
 class FeatureItem extends Component {
     render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const options = this.props.features[feature].map(item => {
+              console.log(this.props.features)
+              const itemHash = slugify(JSON.stringify(item));
+      
+              return (
+                <div key={itemHash} className="feature__item">
+                  <input
+                    type="radio"
+                    id={itemHash}
+                    className="feature__option"
+                    name={slugify(feature)}
+                    checked={item.name === this.props.selected[feature].name}
+                    onChange={e => this.updateFeature(feature, item)}
+                  />
+                  <label htmlFor={itemHash} className="feature__label">
+                    {item.name} ({USCurrencyFormat.format(item.cost)})
+                  </label>
+                </div>
+              )};
+          
+            
+         
+    
 }
-
+    }}
 export default FeatureItem;
